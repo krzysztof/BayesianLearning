7ed29b885ac54fcf15063a8ca402972818da0dd7
Krzysztof Nowak <kiryx7@gmail.com>
2012-10-15 16:58:19 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Learning of Canonical networks
+# author: Krzysztof Nowak
+
+def main():
+
+	filename = "Network1.txt"
+	file = open("Network1.txt")
+
+	for line in file.read():
+		print line
+
+if __name__ == "__main__":
+	main()
